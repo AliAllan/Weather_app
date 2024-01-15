2d16232647a5c10999264eed3743eb8de9e3cf91
Ali Allan <aliallan9931@gmail.com>
2024-01-15 15:05:08 +0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = "7cebcde724fc3ef15a5ac8a0745dea3c";
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6db2c969aemsh169b8bb95d04746p1e0766jsna04f1455a828',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
